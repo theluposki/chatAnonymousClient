f01fa637509d599c50dc7998552d93e4d16ff641
theluposki <theluposki@gmail.com>
2023-08-13 03:12:53 -0300

    list conversations

---
+++ src/mock/listCoversations.js
@@ -0,0 +1,62 @@
+export const listCoversations = [
+    { 
+      id: 1, 
+      nickname: "Mylena",
+      lastMessage: "Oi amor ❤️",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 2, 
+      nickname: "Monique",
+      lastMessage: "e ai jao",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 3, 
+      nickname: "Gustavo",
+      lastMessage: "bora pro rolê?",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 4, 
+      nickname: "Marcela",
+      lastMessage: "suav cachorro 🐕",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 5, 
+      nickname: "Manuela",
+      lastMessage: "tuka ...",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 6, 
+      nickname: "Mylena",
+      lastMessage: "Oi amor ❤️",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 7, 
+      nickname: "Monique",
+      lastMessage: "e ai jao",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 8, 
+      nickname: "Gustavo",
+      lastMessage: "bora pro rolê?",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 9, 
+      nickname: "Marcela",
+      lastMessage: "suav cachorro 🐕",
+      createdAt: "há 10 min"
+    },
+    { 
+      id: 10, 
+      nickname: "Manuela",
+      lastMessage: "tuka ...",
+      createdAt: "há 10 min"
+    },
+  ]
--- src/views/conversations.vue
+++ src/views/conversations.vue
@@ -1,3 +1,126 @@
+<script setup>
+import { ref, onMounted } from "vue"
+import { listCoversations } from "../mock/listCoversations.js"
+  
+const list = ref(listCoversations)
+
+</script>
+
 <template>
-  <h1>conversations</h1>
+  <ul class="list">
+    <li class="item" v-for="(item, i) in list" :key="item.id">
+      <div class="wrapper-image">
+        <img :src="'https://i.pravatar.cc/512?img='+i" class="image" alt="image" />
+      </div>
+      <div class="details">
+        <span class="time">
+          {{ item.createdAt }}
+        </span>
+        <h3>{{ item.nickname }}</h3>
+        <span class="lastMessage">
+          {{ item.lastMessage }}
+        </span>
+      </div>
+    </li>
+    <li class="end-list">fim da lista</li>
+  </ul>
 </template>
+
+<style scoped>
+  .list {
+    position: relative;
+    list-style: none;
+    background-color: var(--dark);
+    min-height: calc(100vh - 60px);
+    max-height: calc(100vh - 60px);
+    overflow-x: hidden;
+    overflow-y: auto;
+    
+    padding-top: 8px;
+    padding-bottom: 80px;
+    
+    display: flex;
+    flex-flow: column;
+    gap: 4px;
+  }
+  
+  .end-list {
+    text-align: center;
+    color: #ddd;
+  }
+  
+  .item {
+    min-height: 80px;
+    background-color: var(--dark2);
+    
+    display: flex;
+    align-items: center;
+    
+    border-radius: 6px;
+    box-shadow: inset 0 0 3px black;
+    overflow: hidden;
+    padding: 4px;
+  }
+  
+  .item:active {
+    scale: 0.95;
+  }
+  
+  .wrapper-image {
+    width: 70px;
+    height: 70px;
+    border-radius: 8px;
+    object-fit: cover;
+    margin-left: 4px;
+  }
+  
+  .image {
+    width: 70px;
+    height: 70px;
+    border-radius: 8px;
+    object-fit: cover;
+  }
+  
+  .item h3 {
+    flex: 1;
+    min-width: 150px;
+    display: flex;
+    align-items: center;
+    padding: 0 14px;
+    font-size: 22px;
+  }
+  
+  
+  .details {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    min-height: 70px;
+    max-height: 70px;
+    overflow: hidden;
+  }
+  
+  .lastMessage {
+    height: 20px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+    color: var(--white2);
+    padding: 8px 14px;
+    font-size: 14px;
+  }
+  
+  .time {
+    height: 20px;
+    max-height: 20px;
+    font-size: 12px;
+    padding: 0 14px;
+    
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    color: #ddd;
+  }
+</style>
